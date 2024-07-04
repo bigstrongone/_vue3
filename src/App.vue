54b9041fe3bcd7427c5caf6d5cc2a847a10ac8c1
gpf <2803401424@qq.com>
2024-07-04 20:41:55 +0800

    重新提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import { useUserStore } from '@/stores/modules/user.js'
+import zh from 'element-plus/es/locale/lang/zh-cn.mjs'
+
+// const userStore = useUserStore()
+</script>
+
+<template>
+  <!-- 国际化处理 -->
+  <el-config-provider :locale="zh">
+    <router-view />
+  </el-config-provider>
+</template>
+
+<style scoped></style>
